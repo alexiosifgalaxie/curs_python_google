04a5d7542ce8324d7945bff690af57e0084feaa8
alexiosifgalaxie <alexandruiosif187@gmail.com>
2021-11-08 18:01:31 +0200

    mesaj

--- Curs2.py
+++
@@ -1,8 +0,0 @@
-#for item, value in enumerate(lista_data)
-#    print(f"{item} => {value}")
-#functionalitate de for 'normal'
-
-#keys,balues,itwms la dictionare, intelegi tu
-
-#DACA APUCI SA CITESTI, NU PUTEAM SA SCRIU IN RITM CU TINE SI SA FIU SI ATENT, AM PREFERAT SA URMARESC CUM EXPLICI :))
-#Fac commit ca sa vad cum imi merge
