7e3b93c5a84d104e73508df12f6d9a233f62aa15
Alexlaptop <alexandruiosif187@gmail.com>
2021-11-15 19:48:51 +0200

    tema webscraping

--- webscraping_covid.py
+++ webscraping_covid.py
@@ -6,17 +6,27 @@ import pandas as pd
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.get("https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-14-noiembrie-ora-13-00-2/")
-table = browser.find_element_by_xpath('//table[@width="0"]')
-table_text = table.text
-lista = table_text.split('\n')
-print(lista)
-header = browser.find_element(By.XPATH, '//table[@width="0"]/tbody/tr').text.split('\n')
-dictionar = {i: [] for i in header}
-for j in range(0, len(header)):
-    for i in range(len(header) + int(j), len(lista), len(header)):
-        dictionar[header[int(j)]].append(lista[i])
-print(dictionar)
+judete = browser.find_elements(By.XPATH, '//table[@width="0"]/tbody/tr/td[@width="151"]')
+lista_judete= [i.text for i in judete]
+lista_judete.append("TOTAL")
 
+dictionar = {lista_judete[0]: lista_judete[slice(1, len(lista_judete))]}
+for i in range(14,7,-1):
+    browser.get("https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-"+str(i)+"-noiembrie-ora-13-00-2/")
+    if not browser.find_elements(By.XPATH, '//table[@width="0"]/tbody/tr/td[@width="189"]'):
+        continue
+    column1 = browser.find_elements(By.XPATH, '//table[@width="0"]/tbody/tr/td[@width="189"]')
+    column2 = browser.find_elements(By.XPATH, '//table[@width="0"]/tbody/tr/td[@width="161"]')
+    column3 = browser.find_elements(By.XPATH, '//table[@width="0"]/tbody/tr/td[@width="162"]')
+    lista1 = [i.text for i in column1]
+    lista1[0] = lista1[0] + f"({i}.noiembrie)"
+    lista2 = [i.text for i in column2]
+    lista2[0] = lista2[0] + f"({i}.noiembrie)"
+    lista3 = [i.text.replace(',', '.') for i in column3]
+    lista3[0] = lista3[0] + f"({i}.noiembrie)"
+    dictionar[lista1[0]] = lista1[slice(1, len(lista1))]
+    dictionar[lista2[0]] = lista2[slice(1, len(lista2))]
+    dictionar[lista3[0]] = lista3[slice(1, len(lista3))]
 df = pd.DataFrame(dictionar)
 df.to_csv("covid.csv")
 time.sleep(2)
